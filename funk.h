a548c5a7bb26d12dd7fe7a5e96da4febbd5c2019
miri-lo <122477386+miri-lo@users.noreply.github.com>
2026-01-10 18:18:32 +0100

    Create funk.h

---
+++ funk.h
@@ -0,0 +1,22 @@
+/*
+ * funk.h
+ *
+ *  Created on: Dec 8, 2025
+ *      Author: gothi
+ */
+
+#ifndef INC_FUNK_H_
+#define INC_FUNK_H_
+
+
+
+#endif /* INC_FUNK_H_ */
+
+#include "main.h"
+
+
+
+void transmit(int numHighPulses, int numLowPulses);
+void sendSequence(char* bitSequenz);
+void delay350Microseconds(uint32_t n);
+void stecker_schalten ();
